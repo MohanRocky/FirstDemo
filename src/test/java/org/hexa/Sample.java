07691293e34133e7c588b0e1042807bf0214f7fa
MohanRocky <mohanv20394@gmail.com>
2021-04-14 21:10:52 +0530

    New task

---
+++ src/test/java/org/hexa/Sample.java
@@ -0,0 +1,17 @@
+package org.hexa;
+
+public class Sample {
+
+private void method1() {
+	System.out.println("Vegetables");
+}		
+
+private void method2() {
+	System.out.println("Fruits");
+}
+
+private void method3() {
+System.out.println("Icecream");
+}
+	}
+
